6f330d7b120925ad0523251e9f7783df1bb6f215
Kyle Adkins <kyle.m.adkins@gmail.com>
2023-12-13 23:46:41 -0500

    Recreate ChatGPT in the terminal using LangChain memory

---
+++ main.py
@@ -0,0 +1,35 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+from langchain.prompts import (
+    MessagesPlaceholder,
+    HumanMessagePromptTemplate,
+    ChatPromptTemplate,
+)
+from langchain.memory import ConversationBufferMemory
+from dotenv import load_dotenv
+
+load_dotenv()
+
+chat = ChatOpenAI()
+
+# return_messages=True formats the messages
+# for chat-based models instead of regular strings
+# i.e. HumanMessage, AIMessage
+memory = ConversationBufferMemory(memory_key="messages", return_messages=True)
+
+prompt = ChatPromptTemplate(
+    input_variables=["content", "messages"],
+    messages=[
+        # Tells LangChain to look in the memory input
+        # for a variable named "messages"
+        MessagesPlaceholder(variable_name="messages"),
+        HumanMessagePromptTemplate.from_template("{content}"),
+    ],
+)
+
+chain = LLMChain(llm=chat, prompt=prompt, memory=memory)
+
+while True:
+    content = input("> ")
+    result = chain({"content": content})
+    print(result["text"])
